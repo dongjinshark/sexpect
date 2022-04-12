55166b61e824bea1633a7af713a0483c49ecdc25
gaoyupeng <gaoyupeng@haomo.ai>
2022-04-12 17:20:44 +0800

    ssh

--- examples/ssh.sh
+++ examples/ssh.sh
@@ -10,36 +10,32 @@ function fatal()
     exit 1
 }
 
-if [[ $# -lt 3 || $1 != -p ]]; then
-    fatal "Usage: $0 -p PASSWORD [USER@]HOST [COMMAND ...]"
-fi
+#if [[ $# -lt 3 || $1 != -p ]]; then
+#    fatal "Usage: $0 -p PASSWORD [USER@]HOST [COMMAND ...]"
+#fi
 
-passwd=$2; shift 2
+#passwd=$2; shift 2
+
+input_host=$1
+input_token=$(oathtool --totp -b -d 6 $MFA_KEY)
 
 export SEXPECT_SOCKFILE=/tmp/sexpect-ssh-$$.sock
 
 type -P sexpect >& /dev/null || exit 1
 
-sexpect spawn -idle 60 -t 10 \
-    ssh -o PreferredAuthentications=keyboard-interactive,password \
-        -o NumberOfPasswordPrompts=1 \
-        -o StrictHostKeyChecking=no \
-        -o UserKnownHostsFile=/dev/null \
-        -o ControlPath=none \
-        -o LogLevel=ERROR \
-        "$@"
+sexpect spawn -idle 60 -t 10 ssh -o strictHostKeyChecking=no -p 6688 $JUMP_SERVER
 
 while true; do
-    sexpect expect -nocase -re 'password:|yes/no'
+    sexpect expect -nocase -re 'password:|OTP Code'
     ret=$?
     if [[ $ret == 0 ]]; then
         out=$( sexpect expect_out )
-        if [[ $out == yes/no ]]; then
-            sexpect send -enter yes
-            continue
-        else
-            sexpect send -enter "$passwd"
+        if [[ $out == "OTP Code" ]]; then
+            sexpect send -enter "$input_token"
             break
+        else
+            sexpect send -enter "$PASSWORD"
+            continue
         fi
     elif sexpect chkerr -errno $ret -is eof; then
         sexpect wait
